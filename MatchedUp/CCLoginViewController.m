8a2e8f90ce19d61c02a01f774fe573149e0066bf
MTTHWBSH <bushmat4@gmail.com>
2014-09-19 23:00:22 -0500

    fixing all parse versioning issues, returning dictionary of user data

--- MatchedUp/CCLoginViewController.m
+++ MatchedUp/CCLoginViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "CCLoginViewController.h"
+#import <Parse/Parse.h>
+#import <FacebookSDK/FacebookSDK.h>
 
 @interface CCLoginViewController ()
 
@@ -19,6 +21,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.activityIndicator.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,7 +32,68 @@
 
 #pragma mark - IBActions
 - (IBAction)loginButtonPressed:(UIButton *)sender {
+    self.activityIndicator.hidden = NO;
+    [self.activityIndicator startAnimating];
+    NSArray *permissionsArrary = @[@"user_about_me", @"user_interests", @"user_relationships", @"user_birthday", @"user_location", @"user_relationship_details"];
+    [PFFacebookUtils logInWithPermissions:permissionsArrary block:^(PFUser *user, NSError *error) {
+        [self.activityIndicator stopAnimating];
+        self.activityIndicator.hidden = YES;
+        if (!user) {
+            if (!error) {
+                UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:@"The Facebook login was cancelled" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+                [alertview show];
+            }
+            else {
+                UIAlertView *alerview = [[UIAlertView alloc] initWithTitle:[error description] message:nil delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles: nil];
+                [alerview show];
+            }
+        }
+        else    {
+            [self updateUserInformation];
+            [self performSegueWithIdentifier:@"loginToTabBarSegue" sender:self];
+        }
+    }];
+}
+
+#pragma mark - Helper Method
+
+- (void)updateUserInformation
+{
+    FBRequest *request = [FBRequest requestForMe];
     
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        
+        NSLog(@"%@", result);
+        
+        if (!error) {
+            NSDictionary *userDictionary = (NSDictionary *)result;
+            NSMutableDictionary *userProfile = [[NSMutableDictionary alloc] initWithCapacity:8];
+            if (userDictionary[@"name"]) {
+                userProfile[@"name"] = userDictionary[@"name"];
+            }
+            if (userDictionary[@"first_name"]) {
+                userProfile[@"first_name"] = userDictionary[@"first_name"];
+            }
+            if (userDictionary[@"location"][@"name"]) {
+                userProfile[@"location"] = userDictionary[@"location"][@"name"];
+            }
+            if (userDictionary[@"gender"]) {
+                userProfile[@"gender"] = userDictionary[@"gender"];
+            }
+            if (userDictionary[@"birthday"]) {
+                userProfile[@"birthday"] = userDictionary[@"birthday"];
+            }
+            if (userDictionary[@"interested_in"]) {
+                userProfile[@"interested_in"] = userDictionary[@"interested_in"];
+            }
+            
+            [[PFUser currentUser] setObject:userProfile forKey:@"profile"];
+            [[PFUser currentUser] saveInBackground];
+        }
+        else    {
+            NSLog(@"Error in Facebook request %@", error);
+        }
+    }];
 }
 
 @end
--- MatchedUp/FirstViewController.m
+++ MatchedUp/FirstViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FirstViewController.h"
+#import <Parse/Parse.h>
 #import <FacebookSDK/FacebookSDK.h>
 
 @interface FirstViewController ()
